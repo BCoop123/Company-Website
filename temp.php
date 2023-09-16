1d166120e74fe132899e7fd945964f99a7b4b3e4
BCoop123 <brandoncooper859@gmail.com>
2023-09-16 00:32:04 -0400

    added function for reading plaintext

---
+++ temp.php
@@ -0,0 +1,33 @@
+<?php
+    require_once('./lib/read_plaintxt.php');
+    require_once('./lib/read_json.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Function Tester</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+    <h2>PHP Function Tester</h2>
+    <?php
+        $plainText = readPlainTextData('./data/data.txt');
+        $jsonData = readJsonData('./data/data.json');
+
+        echo $plainText;
+        //print_r($jsonData);
+        echo $jsonData['books'][0]['id'];
+    ?>
+</div>
+
+<!-- Bootstrap JS and dependencies -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
