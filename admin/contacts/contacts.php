0450299f2c28d76910bca35081e9832f3a44b6d8
BCoop123 <brandoncooper859@gmail.com>
2023-10-01 21:02:30 -0400

    created index page for contacts

---
+++ admin/contacts/contacts.php
@@ -0,0 +1,73 @@
+<?php
+// Creates a table that contains file names and contents of a folder
+function createTable($headings, $files) {
+    echo '
+    <div class="container-fluid">
+        <!-- Bootstrap table -->
+        <table class="table" style="width: 90%; margin: auto;">
+            <thead>
+                <tr>
+    ';
+                    foreach ($headings as $key => $heading) {
+                        echo '
+                            <th>' . $heading . '</th>
+                        ';
+                    };
+    echo '
+                </tr>
+            </thead>
+            <tbody>
+    ';
+                    foreach ($files as $key => $file) {
+                        echo '
+                            <tr>
+                                <td><a href="./detail.php?file=' . $file[0] . '">' . $file[0] . '</a></td>
+                                <td>' . $file[1] . '</td>
+                                <td>' . $file[2] . '</td>
+                            </tr>
+                        ';
+                    };
+    echo '
+            </tbody>
+        </table>
+    </div>
+    ';
+}
+
+function getContactsInfo($dir_path) {
+    $contactsArray = [];
+
+    // Open the directory
+    $dir_handle = opendir($dir_path);
+
+    if ($dir_handle) {
+        while (false !== ($filename = readdir($dir_handle))) {
+            $file_path = $dir_path . "/" . $filename;
+
+            // Check if it's a file and not a directory
+            if (is_file($file_path)) {        
+                // Fetch and decode the contents of the file
+                $contents = file_get_contents($file_path);
+                $contacts = json_decode($contents, true); // true for associative array
+
+                if (is_array($contacts) && !empty($contacts)) {
+                    foreach ($contacts as $contact) {
+                        $contactsArray[] = [
+                            $contact["name"],
+                            $contact["email"],
+                            $contact["subject"]
+                        ];
+                    }
+                }
+            }
+        }
+
+        // Close the directory handle
+        closedir($dir_handle);
+    } else {
+        echo "Failed to open directory!";
+    }
+    return $contactsArray;
+}
+
+?>
